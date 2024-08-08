3b5fc046585556540de8e9be2e881dfd87f4537a
oBe3yH4uKo <androuser365@gmail.com>
2024-08-08 13:23:17 +0300

    Add files via upload

---
+++ 1st_practic.py
@@ -0,0 +1,17 @@
+# 1st programm
+print(9**0.5 * 5)
+# 2nd programm
+print(9.99>9.98 and 1000!=1000.1)
+#3rd programm
+a=2*2+2
+b=2*(2+2)
+print(a)
+print(b)
+print(a==b)
+#4th programm
+str_a = '123.456'
+b = 10
+c = float(str_a) * b
+x = int(c) % 10
+print(x)
+
